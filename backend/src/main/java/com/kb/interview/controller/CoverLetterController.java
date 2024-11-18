051acf5b9833447801226addecc0304ec76c60a6
ming-taro <hera002008@naver.com>
2024-11-19 00:08:38 +0900

    feat: 자기소개서 생성 및 수정

--- backend/src/main/java/com/kb/interview/controller/CoverLetterController.java
+++ backend/src/main/java/com/kb/interview/controller/CoverLetterController.java
@@ -1,13 +1,13 @@
 package com.kb.interview.controller;
 
-import com.kb.interview.dto.coverletter.CoverLetter;
-import com.kb.interview.dto.coverletter.CoverLetterRequest;
+import com.kb.interview.dto.coverletter.*;
 import com.kb.interview.service.CoverLetterService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.List;
 
 @Api(value = "CoverLetterController", tags = "자기소개서")
@@ -16,9 +16,11 @@ import java.util.List;
 @RequestMapping("/api/cover-letter")
 public class CoverLetterController {
     private final CoverLetterService service;
+
     @PostMapping("")
     public ResponseEntity<CoverLetter> createCoverLetter(@RequestBody CoverLetterRequest coverLetterRequest) {
-        CoverLetter coverLetter = service.create(coverLetterRequest);
+        CoverLetter coverLetter = service.create(coverLetterRequest);          // 자기소개서 생성
+        List<CoverLetterAnswer> answers = service.createAnswers(coverLetter);   // 항목별 답변란 생성
 
         if (coverLetter == null) {
             return ResponseEntity.noContent().build();
@@ -44,5 +46,11 @@ public class CoverLetterController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(coverLetters);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<List<CoverLetterAnswerSaveRequest>> updateCoverLetterAnswer(@RequestBody List<CoverLetterAnswerSaveRequest> answers) {
+        service.updateAnswers(answers);
+        return ResponseEntity.ok(answers);
     }
 }
