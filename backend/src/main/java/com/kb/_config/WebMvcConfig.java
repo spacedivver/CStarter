1df4fed067c9eb4f5fa9cf862214a76a0df86c5e
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-11-18 19:43:41 +0900

    fix: CORS 허용

--- backend/src/main/java/com/kb/_config/WebMvcConfig.java
+++ backend/src/main/java/com/kb/_config/WebMvcConfig.java
@@ -7,12 +7,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.kb")
 public class WebMvcConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // CORS를 적용할 URL 패턴
+                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowCredentials(true); // 인증 정보를 포함한 요청 허용
+    }
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
